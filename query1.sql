50172f3592d8b891ff1c807012d501fbb9423036
terrylarch <76491897+terrylarch@users.noreply.github.com>
2022-08-05 11:41:18 -0400

    Add files via upload
    First upload version of query1_test

---
+++ query1.sql
@@ -0,0 +1,14 @@
+# Reveal exsisting databases
+SHOW DATABASES;
+
+/*
+Create a new database called "my_new_database" only if
+a database does not already exist with that name
+*/
+
+CREATE DATABASE IF NOT EXISTS my_new_database;
+
+-- Reveal all databases
+SHOW DATABASES;
+
+
